1ba054bde47ee0edff2c69761a98cc952dc49ed0
swic <1499718389@qq.com>
2021-10-17 21:01:31 +0800

    update Person and UserController  commit by master Two

--- src/main/java/com/atguigu/admin/controller/UserController.java
+++ src/main/java/com/atguigu/admin/controller/UserController.java
@@ -23,9 +23,9 @@ public class UserController {
 	
 	@ResponseBody
 	@GetMapping("/person")
-	public Person person() {
+	public Person person(){
 
-		System.out.println("45665446546");
+
        Person person = new Person(1001, "swic", 22, "深圳");
 
 		return person;
